9818128b04f4725d71ce76818fa5ea57e1cfaea0
Carlos Planchart <carlos_planchart@live.com>
2022-07-15 17:48:15 +0200

    added watercolor layer to geoJson exampe

--- Geojson-Example -Barcelona/index.js
+++ Geojson-Example -Barcelona/index.js
@@ -1,6 +1,8 @@
 var map = L.map("map").setView([41.3947, 2.1698], 13);
 
-var tiles = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+
+
+var tiles = L.tileLayer("https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg", {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
